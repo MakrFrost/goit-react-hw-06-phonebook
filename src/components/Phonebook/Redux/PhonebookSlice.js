e5633c8e7e96c9651a0a5e88765931b027597f17
MakrFrost <frostmark@ukr.net>
2022-12-07 17:59:36 +0200

    Update PhonebookSlice.js

--- src/components/Phonebook/Redux/PhonebookSlice.js
+++ src/components/Phonebook/Redux/PhonebookSlice.js
@@ -5,12 +5,7 @@ import storage from 'redux-persist/lib/storage';
 export const phonebookSlice = createSlice({
   name: 'contacts',
   initialState: {
-    items: [
-      { id: 'id-1', name: 'Eden Clements', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Rosie Aniston', number: '645-17-79' },
-      { id: 'id-4', name: 'Homer Simpson', number: '227-91-26' },
-    ],
+    items: [],
     filter: '',
   },
   reducers: {
